057034d9d5e331b4d9d9d6deaaaf7a75090d6a45
Enos Namiinda <namiindaenos@gmail.com>
2023-02-17 10:33:00 -0800

    alphaBets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 	char alph;
 
 	for (alph = 'a'; alph <= 'z'; alph++)
-	{
 		putchar(alph);
-		putchar('\n');
-	}
+	for (alph = 'A'; alph <= 'Z'; alph++)
+		putchar(alph);
+	putchar('\n');
 
 	return (0);
 }
