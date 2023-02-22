b51e475ec83dd2a1b1f23d6b28564e43716c0a6b
Enos Namiinda <namiindaenos@gmail.com>
2023-02-21 18:54:41 -0800

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Check description
+ * @c: An input character
+ * Description: function that checks uppercase
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
