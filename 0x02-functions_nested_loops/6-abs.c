3d97b2587d61a13f0c826382b4e939dca51a2420
Enos Namiinda <namiindaenos@gmail.com>
2023-02-22 07:23:47 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - Check description
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolute value of number r
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+		return (r);
+	}
+}
